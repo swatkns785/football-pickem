5621c2563c52ced859f988e865a246a7970cb545
Steven Watkins <steven.watkins78@gmail.com>
2015-04-25 17:11:10 -0400

    added user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name,
+    presence: true
+
+  validates :last_name,
+    presence: true
+
+  validates :username,
+    presence: true
+
+  validates :email,
+    presence: true,
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :sign_up }
+
+  validates :password,
+    presence: true
+
+  validates :password_confirmation,
+    presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should have_valid(:first_name).when('Joe', 'Michael') }
+  it { should_not have_valid(:first_name).when(nil, '') }
+
+  it { should have_valid(:last_name).when('Sample', 'Mitchell') }
+  it { should_not have_valid(:last_name).when(nil, '') }
+
+  it { should have_valid(:username).when('joesample1', 'mikey1234') }
+  it { should_not have_valid(:username).when(nil, '') }
+
+  it { should have_valid(:email).when('joesample@gmail.com',
+    'someone@yahoo.com') }
+  it { should_not have_valid(:email).when(nil, 'word', '.com', 'word@com',
+    'word.com' )}
+
 end
