2229c376e49a6493fee648d854132fe942f4dbcd
Steven Watkins <steven.watkins78@gmail.com>
2015-05-10 00:05:28 -0400

    uncommmented week creating seed lines

--- spec/models/week_spec.rb
+++ spec/models/week_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Week, type: :model do
   it { should have_many :league_weeks }
+  it { should have_many :games }
 
   it { should have_valid(:week_number).when(1, 2, 3) }
   it { should_not have_valid(:week_number).when("", 3.5) }
