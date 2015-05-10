440cf4a3c71df9a33e79d85d700fc63a44de583c
Steven Watkins <steven.watkins78@gmail.com>
2015-05-10 15:49:31 -0400

    wrote method to create league_games upon league creation

--- app/models/league.rb
+++ app/models/league.rb
@@ -7,6 +7,7 @@ class League < ActiveRecord::Base
 
   after_create :create_league_weeks
   after_create :create_league_owner
+  after_create :create_league_games
 
   validates :title,
     presence: true,
@@ -16,9 +17,9 @@ class League < ActiveRecord::Base
   #   memberships.find_by(user_id: user.id, league_id: params[:id])
   # end
 
-  def is_league_owner
-    current_user && Membership.find_by(league: params[:id], owner: true)
-  end
+  # def is_league_owner
+  #   current_user && Membership.find_by(league: params[:id], owner: true)
+  # end
 
   private
 
@@ -30,5 +31,25 @@ class League < ActiveRecord::Base
 
   def create_league_owner
     Membership.create(owner: true, user: League.last.user, league: self)
+  end
+
+  def create_league_games
+    games = Game.all
+    league_weeks = LeagueWeek.where(league_id: self.id)
+
+    games.each do |g|
+      league_weeks.each do |lw|
+        if g.week_id == lw.week_id
+          LeagueGame.create(league_id: lw.league_id,
+                            league_week_id: lw.id,
+                            week_id: g.week_id,
+                            day: g.day,
+                            date: g.date,
+                            visitor_team: g.visitor_team,
+                            home_team: g.home_team,
+                            time: g.time)
+        end
+      end
+    end
   end
 end
