25805048c740aebee8a7982a09a53ffcabb5904f
Steven Watkins <steven.watkins78@gmail.com>
2015-05-13 15:38:24 -0400

    WIP form hopefully formatted correctly

--- app/controllers/league_picks_controller.rb
+++ app/controllers/league_picks_controller.rb
@@ -1,7 +1,13 @@
 class LeaguePicksController < ApplicationController
 
   def new
+    @league_week = LeagueWeek.find(params[:league_week_id])
     @league_games = LeagueGame.where(league_week_id: params[:league_week_id])
+    @league_picks = LeaguePick.new
+  end
+
+  def create
+
   end
 
 end
